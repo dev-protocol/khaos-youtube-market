e0e583d638696c09cd19edb3bb2f2050f9d99b2b
CryptoDogg <d.enkikairo@gmail.com>
2021-11-17 15:25:06 +0900

    lint

--- src/addresses.ts
+++ src/addresses.ts
@@ -4,5 +4,5 @@ export const addresses: FunctionAddresses = async ({ network: net }) =>
 	net === 'ropsten'
 		? '0xEd456cE8c099ec4862435B7ec5F2367d40017040'
 		: net === 'arbitrum-rinkeby'
-			? '0x9bCb3F85b0b01968F5e0100652a53958c8558452'
-			: undefined
+		? '0x9bCb3F85b0b01968F5e0100652a53958c8558452'
+		: undefined
